fea58d11721f68ea80bf4aa16c061d0c5ddf46af
Bastian Meissner <meissner.bastian@gmail.com>
2022-03-13 21:59:47 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,5 +4,10 @@
 git pull
 git submodule update --init
 
-# Install all packages
-stow -v -t $HOME -R */
+if [ -n $1 ] && [ "$1" != "linux" ]; then
+    # Install all packages, excluding macOS specifics.
+    stow -v -t $HOME -R vim zsh
+else
+    # Install all packages
+    stow -v -t $HOME -R vim zsh macos
+fi
