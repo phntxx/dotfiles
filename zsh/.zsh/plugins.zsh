76f95cdf552b476bbaf80914adf56f2f7edf62a6
Bastian Meissner <meissner.bastian@gmail.com>
2022-04-27 09:21:14 +0200

    Update dotfiles

--- zsh/.zsh/plugins.zsh
+++ zsh/.zsh/plugins.zsh
@@ -2,8 +2,9 @@
 ZSH_PLUGIN_DIR=~/.zsh/plugins
 
 # Source powerlevel10k and configuration
-source $ZSH_PLUGIN_DIR/powerlevel10k/powerlevel10k.zsh-theme
-source $ZSH_PLUGIN_DIR/.p10k.zsh
+# source $ZSH_PLUGIN_DIR/powerlevel10k/powerlevel10k.zsh-theme
+# source $ZSH_PLUGIN_DIR/.p10k.zsh
+eval "$(starship init zsh)"
 
 # Source zsh-autosuggestions, zsh-syntax-highlighing and zsh-history-substring-search
 source $ZSH_PLUGIN_DIR/zsh-autosuggestions/zsh-autosuggestions.zsh
