48c65420336127ccc3f67afc28e09d4c4e4c3501
Bastian Meissner <phntxx@users.noreply.github.com>
2022-10-04 10:01:20 +0200

    Add helix

--- zsh/aliases/aliases.zsh
+++ zsh/aliases/aliases.zsh
@@ -4,5 +4,11 @@ alias ls='lsd'
 alias la='lsd -la'
 alias ll='lsd -ll'
 
+# `vi`/`vim`-related aliases
+# Friendship ended with vi/vim, now helix is my best friend
+alias vi='hx'
+alias vim='hx'
+alias nano='hx'
+
 # other aliases
 alias reset_zcompdump='rm -f ~/.zcompdump; compinit'
